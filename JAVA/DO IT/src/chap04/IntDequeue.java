464650340f6ca01e0a220031629bd46e62a9d3d9
wjh.laptop <jwon3991.9191@gmail.com>
2019-11-30 15:08:53 +0900

    DO IT LastNelements

---
+++ JAVA/DO IT/src/chap04/IntDequeue.java
@@ -0,0 +1,41 @@
+package chap04;
+
+public class IntDequeue {
+	private int max; // 덱(deck)의 용량
+	private int num; // 현재의 데이터 수
+	private int front; // 맨 앞 커서
+	private int rear; // 맨 뒤 커서
+	private int[] que; // 덱(deck)의 본체
+	
+	
+	// 덱(deck)에 데이터를 머리쪽부터 인큐
+		public int enqueFront(int x)  {
+			return 0;
+		}
+
+		// 덱(deck)에 데이터를 꼬리쪽부터 인큐
+		public int enqueRear(int x) {
+			return 0;
+		}
+	// 덱(deck)의 머리부터 데이터를 디큐
+		public int dequeFront()  {
+			
+			return 0;
+		}
+
+		// 덱(deck)의 꼬리부터 데이터를 디큐
+		public int dequeRear()  {
+			return 0;
+		}
+
+		// 덱(deck)의 머리부터 데이터를 피크(머리데이터를 살펴봄)
+		public int peekFront() {
+			return 0;
+		}
+
+		// 덱(deck)의 꼬리부터 데이터를 피크(꼬리데이터를 살펴봄)
+		public int peekRear()  {
+			return 0;
+		}
+	
+}
